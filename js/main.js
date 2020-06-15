eaef47629cd83329c37a88418938989fe7875c52
Luiz Gustavo Bourscheit <oluizgustavob@gmail.com>
2020-06-15 13:13:39 -0300

    fix css

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.querySelector("#view").innerHTML += `
   ${narrations.map((narration, index) => `
       <div class="narration">
         <div class="narration__info">
-          <p class="narration_info--game">${narration.game}</p>
+          <p class="narration__info--game">${narration.game}</p>
           <p>${narration.goal}</p>  
         </div>
         <div class="narration__audio">
